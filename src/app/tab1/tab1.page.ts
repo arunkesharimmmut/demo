8aae5df5e32c43faaa12d4ebd4a38007894e0771
Arun <arunkesharri@gmail.com>
2021-05-10 17:18:33 +0530

    first commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {users: any;
+  data: any;
+
+  constructor() {}
+
+  ionViewWillEnter() {
+    setTimeout(() => {
+      this.data = {
+        'heading': 'Normal text',
+        'para1': 'Lorem ipsum dolor sit amet, consectetur',
+        'para2': 'adipiscing elit.'
+      };
+    }, 5000);
+  }
+}
